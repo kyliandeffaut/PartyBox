250d33ec98c86bd63d54d3abb7451cd1e9931665
Kylian <kylian.deffaut@gmail.com>
2026-04-19 21:07:06 +0200

    background

--- lib/main.dart
+++ lib/main.dart
@@ -59,11 +59,15 @@ class _PlayerScreenState extends State<PlayerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF0f0c29), Color(0xFF302b63), Color(0xFF24243e)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+        decoration: BoxDecoration(
+          color: const Color(0xFF101012), 
+          image: DecorationImage(
+            image: const AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover, // Prends tout l'écran
+            colorFilter: ColorFilter.mode(
+              Colors.black.withValues(alpha: 0.5), // Ajuste l'alpha (0.0 à 1.0) pour assombrir plus ou moins
+              BlendMode.darken,
+            ),
           ),
         ),
         child: SafeArea(
@@ -122,7 +126,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
                   .slideX(begin: 0.5, end: 0, curve: Curves.easeOutBack),
                 ),
               ),
-
+              
               if (players.length >= 2)
                 Padding(
                   padding: const EdgeInsets.all(20),
@@ -132,15 +136,18 @@ class _PlayerScreenState extends State<PlayerScreen> {
                       width: double.infinity,
                       height: 60,
                       decoration: BoxDecoration(
-                        color: const Color(0xFF101012), 
-                        image: DecorationImage(
-                          image: const AssetImage('assets/images/background.jpg'), // 👈 Remplace par le bon nom !
-                          fit: BoxFit.cover, // Prends tout l'écran
-                          colorFilter: ColorFilter.mode(
-                            Colors.black.withValues(alpha: 0.5), // Ajuste l'alpha (0.0 à 1.0) pour assombrir plus ou moins
-                            BlendMode.darken,
-                          ),
+                        gradient: const LinearGradient(
+                          colors: [Color(0xFF00C6FF), Color(0xFF0072FF)], 
                         ),
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blue.withValues(alpha: 0.5), 
+                            blurRadius: 20, 
+                            spreadRadius: 2,
+                            offset: const Offset(0, 5)
+                          )
+                        ],
                       ),
                       child: const Center(
                         child: Text(
