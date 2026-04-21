8f41ec3c59dc34d6b240a534ec8b47f889f51598
Kylian <kylian.deffaut@gmail.com>
2026-04-21 11:01:36 +0200

    magique button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -72,29 +72,30 @@ class _HomeScreenState extends State<HomeScreen> {
                     const SnackBar(content: Text("✨ Modes Premium débloqués à vie !"), backgroundColor: Colors.green),
                   );
                 }
-                // NOUVEAU CODE POUR REVERROUILLER
-                else if (secretController.text == "RESET") {
-                  final prefs = await SharedPreferences.getInstance();
-                  await prefs.setBool('isPremium', false); // On remet la mémoire à false
-                  
-                  if (context.mounted) {
-                    setState(() => _isPremiumUnlocked = false); // On met à false pour l'affichage
-                    Navigator.pop(context);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("🔒 Premium désactivé"), backgroundColor: Colors.orange),
-                    );
-                  }
-                }
-                // MAUVAIS CODE
-                } else {
+              }
+              // NOUVEAU CODE POUR REVERROUILLER
+              else if (secretController.text == "RESET") {
+                final prefs = await SharedPreferences.getInstance();
+                await prefs.setBool('isPremium', false); // On remet la mémoire à false
+                
+                if (context.mounted) {
+                  setState(() => _isPremiumUnlocked = false); // On met à false pour l'affichage
                   Navigator.pop(context);
                   ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text("❌ Code invalide."), backgroundColor: Colors.red),
+                    const SnackBar(content: Text("🔒 Premium désactivé"), backgroundColor: Colors.orange),
                   );
                 }
-              },
-              child: const Text("VALIDER", style: TextStyle(color: Colors.pinkAccent)),
-            )
+              }
+              // MAUVAIS CODE
+              else {
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("❌ Code invalide."), backgroundColor: Colors.red),
+                );
+              }
+            },
+            child: const Text("VALIDER", style: TextStyle(color: Colors.pinkAccent)),
+          )
         ],
       ),
     );
